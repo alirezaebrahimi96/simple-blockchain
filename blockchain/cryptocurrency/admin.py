af51c589782fa6d7a1f43b46428eecc3c0ddc1ef
alirezaebrahimi96 <ar.ebrahimi96@gmail.com>
2022-02-07 17:12:28 +0330

    delete

--- blockchain/cryptocurrency/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Node, Transaction, Blockchain
-# Register your models here.
-
-admin.site.register(Node)
-admin.site.register(Transaction)
-admin.site.register(Blockchain)
--- blockchain/cryptocurrency/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CryptocurrencyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cryptocurrency'
